85599fea13769af530edaca79f9cfaac147ba904
shubhkha <shubhamkharat890@gmail.com>
2026-09-13 16:23:51 +0530

    Add database context

---
+++ Project_HRMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Project_Hrms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoHRMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
