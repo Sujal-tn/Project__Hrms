9f97c6144be376185f44e2dd606d2211fa35274b
tanmayneptron <tanmay.p@neptrontech.com>
2026-09-14 21:21:31 +0530

    Added promotion functionality

---
+++ Project_HRMS/Models/Promotion.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_Hrms.Models
+{
+    public class Promotion
+    {
+        [Key]
+        public int PromotionId { get; set; }
+        [Required]
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        [Required]
+        public string DesignationFrom { get; set; }
+        [Required]
+        public string DesignationTo { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
