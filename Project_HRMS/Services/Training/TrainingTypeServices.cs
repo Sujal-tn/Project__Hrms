a4aac9f3d466de3e174f329f2a883a4662b2d820
bagawesampada123-code <bagawesampada123@gmail.com>
2026-09-16 14:36:41 +0530

    training added

---
+++ Project_HRMS/Services/Training/TrainingTypeServices.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Hrms.Data;
+using Project_Hrms.Interface;
+using Project_Hrms.Models;
+
+namespace Project_Hrms.Services
+{
+    public class TrainingTypeServices : ITrainingType
+    {
+        private readonly ApplicationDbContext db;
+        public TrainingTypeServices(ApplicationDbContext db)
+        { 
+            this.db = db;
+        }
+        public async Task AddTrainingType(TrainingType ttype)
+        {
+            await db.TrainingTypes.AddAsync(ttype);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteTrainingType(int id)
+        {
+            var data= await db.TrainingTypes.FindAsync(id);
+            if(data !=null)
+            {
+                db.TrainingTypes.Remove(data);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<TrainingType>> FetchAll()
+        {
+            var data = await db.TrainingTypes.ToListAsync();
+            return data;
+        }
+
+        public async Task<TrainingType?> FindByID(int id)
+        {
+            var data = await db.TrainingTypes.FindAsync(id);
+            return data;
+        }
+
+        public async Task UpdateTrainingType(TrainingType ttype)
+        {
+            db.TrainingTypes.Update(ttype);
+            await db.SaveChangesAsync();
+        }
+    }
+}
