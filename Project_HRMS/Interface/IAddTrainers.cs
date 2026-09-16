83a129b3e0c35455062b62adad749c12c3c09e85
bagawesampada123-code <bagawesampada123@gmail.com>
2026-09-16 19:40:24 +0530

    Added Trainer module

---
+++ Project_HRMS/Interface/IAddTrainers.cs
@@ -0,0 +1,17 @@
+﻿using Project_Hrms.Models;
+
+namespace Project_Hrms.Interface
+{
+    public interface IAddTrainers
+    {
+        Task<List<Trainers>> FetchAll();
+
+        Task<Trainers?> FindByID(int id);
+
+        Task AddTrainer(Trainers t);
+
+        Task UpdateTrainer(Trainers t);
+
+        Task DeleteTrainer(int id);
+    }
+}
