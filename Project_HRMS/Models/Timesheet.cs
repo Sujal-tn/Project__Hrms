7e9c4060cad9d56286a24df9a7b896e4f47ff322
siddhesh3008 <siddhesh.haldankar30@gmail.com>
2026-09-17 13:41:52 +0530

    Timesheet Model Created

---
+++ Project_HRMS/Models/Timesheet.cs
@@ -0,0 +1,30 @@
+﻿using Project_Hrms.Models.EmployeeModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_Hrms.Models
+{
+    public class Timesheet
+    {
+        [Key]
+        public int TimesheetId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Projects")]
+        public int ProjectId { get; set; }
+        public Projects Projects { get; set; }
+
+        public DateTime Date { get; set; }
+        public int WorkHours { get; set; }
+        public string Status { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? ApprovedBy { get; set; }
+        public DateTime? ApprovedAt { get; set; }
+
+    }
+}
